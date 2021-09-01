ca105c663839d4ea6be32d4e6926393cfed8f92f
Prateek <psoni@gammastack.com>
2021-09-01 23:48:31 +0530

    initial commit

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,6 +1,7 @@
 function Header() {
     return(
         <header className="App-header">
+        Test
             <img src='/images/mainLogo.jpg' className="main-logo" alt="logo" />    
             {/* <img src={logo} className="App-logo" alt="logo" /> */}
         </header>
